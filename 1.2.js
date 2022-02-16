4b6786f61b6dc6dbe26932e49a48c367acbd0efd
Axel Siriez <axel.siriez@gmail.com>
2022-02-16 12:12:35 +0100

    exo 1.1 et 1.2

---
+++ 1.2.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+
+let name = "siriez";
+let firstName = "axel";
+let city = "charleroi";
+
+console.log ("je m'appelle",  name , firstName , "et je vis à" , city);
