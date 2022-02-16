81400cad9a36be57462cf140bde3cd9b81b3291e
Axel Siriez <axel.siriez@gmail.com>
2022-02-16 14:23:40 +0100

    exo 1.5

---
+++ 1.5.js
@@ -0,0 +1,6 @@
+const readlineSync = require("readline-sync");
+
+let number1 = new Number (readlineSync.question('give me a decimal number : '));
+let number2 = new Number (readlineSync.question('give me another decimal number : '));
+
+console.log("The result is =" , Math.trunc(number1*number2));
