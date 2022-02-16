f0b59acd973f939c288a3452426d28b5c2e70b16
Axel Siriez <axel.siriez@gmail.com>
2022-02-16 15:36:06 +0100

    exo 1.0

---
+++ 1.0.js
@@ -0,0 +1,8 @@
+let number1 = 2
+let number2 = 3
+console.log(number1+number2)
+console.log(number1-number2)
+console.log(number1*number2)
+console.log(number1/number2)
+console.log(number1%number2)
+.save
