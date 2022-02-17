bd09eeed42b97b3669f149d4da0103e3b130b974
Axel Siriez <axel.siriez@gmail.com>
2022-02-17 09:56:48 +0100

    new exo1.0

--- exo1/1.0.js
+++ exo1/1.0.js
@@ -1,7 +1,7 @@
 let number1 = 2
 let number2 = 3
-console.log(number1+number2)
-console.log(number1-number2)
-console.log(number1*number2)
-console.log(number1/number2)
-console.log(number1%number2)
+console.log("2 + 3 =",number1+number2)
+console.log("2 - 3 =",number1-number2)
+console.log("2 * 3 =",number1*number2)
+console.log("2 / 3 =",number1/number2)
+console.log("2 % 3 =",number1%number2)
