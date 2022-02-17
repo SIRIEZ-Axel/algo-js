f2ecf04a2bf2d542af821937b43966ed39cd0985
Axel Siriez <axel.siriez@gmail.com>
2022-02-17 16:29:15 +0100

    exo 3.3

---
+++ exo3/3.3.js
@@ -0,0 +1,17 @@
+/* écrire un programme qui duplique un tableau */
+/* initialisation du tableau */
+let arr = [1, 2, 3, 4, 5];
+
+/* initialisation de la copie du tableau */
+let arrcopy = [...arr];
+
+console.log(arr, arrcopy);
+
+/* method push */
+let arr2 = ["cerise", "pomme"]
+
+let arrcopy2 = [];
+
+arrcopy2.push(arr2)
+
+console.log(arr2, arrcopy2)
