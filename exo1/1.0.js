e186570bfc84a6d0115c95bc90560b1317fe9198
Axel Siriez <axel.siriez@gmail.com>
2022-02-17 09:52:24 +0100

    nouveau dossier exo1

---
+++ exo1/1.0.js
@@ -0,0 +1,7 @@
+let number1 = 2
+let number2 = 3
+console.log(number1+number2)
+console.log(number1-number2)
+console.log(number1*number2)
+console.log(number1/number2)
+console.log(number1%number2)
