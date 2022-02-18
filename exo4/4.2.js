74fa4524f10d88f738c2cbd002ae3ac734e0f77c
Axel Siriez <axel.siriez@gmail.com>
2022-02-18 12:30:12 +0100

    exo 4.2

---
+++ exo4/4.2.js
@@ -0,0 +1,4 @@
+/* créaction d'une fonction qui retourne un nombre entier entre 1 et 10 */
+let rand10 = Math.floor(Math.random() * 11);
+
+console.log("A random integer between 1 and 10 :", rand10);
