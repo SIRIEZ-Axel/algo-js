bbbf8d2cc8c81ceaee730346a03aefa4df4cc333
SIRIEZ-Axel <98603007+SIRIEZ-Axel@users.noreply.github.com>
2022-02-21 14:46:35 +0100

    Delete 1.2.js

--- 1.2.js
+++
@@ -1,7 +0,0 @@
-const readlineSync = require("readline-sync");
-
-let name = "siriez";
-let firstName = "axel";
-let city = "charleroi";
-
-console.log ("je m'appelle",  name , firstName , "et je vis à" , city);
