21ed551e9d95dded9406c58d54c0d7cb0db79848
SIRIEZ-Axel <98603007+SIRIEZ-Axel@users.noreply.github.com>
2022-02-21 14:47:19 +0100

    Delete 1.6.js

--- 1.6.js
+++
@@ -1,6 +0,0 @@
-const readlineSync = require("readline-sync");
-
-let integer1 = new Number(readlineSync.question('give me an integer number : '))
-let integer2 = new Number(readlineSync.question('give me another integer number : '))
-
-console.log("the rest is",integer1%integer2);
