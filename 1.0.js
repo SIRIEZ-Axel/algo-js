9c4f2b86267d7900e219dea3a0280781de09af3b
SIRIEZ-Axel <98603007+SIRIEZ-Axel@users.noreply.github.com>
2022-02-21 14:46:15 +0100

    Delete 1.0.js

--- 1.0.js
+++
@@ -1,7 +0,0 @@
-let number1 = 2
-let number2 = 3
-console.log(number1+number2)
-console.log(number1-number2)
-console.log(number1*number2)
-console.log(number1/number2)
-console.log(number1%number2)
