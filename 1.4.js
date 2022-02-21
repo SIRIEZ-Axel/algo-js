aada88f1fd706d332d1d914b51259da41439bea7
SIRIEZ-Axel <98603007+SIRIEZ-Axel@users.noreply.github.com>
2022-02-21 14:47:01 +0100

    Delete 1.4.js

--- 1.4.js
+++
@@ -1,7 +0,0 @@
-const readlineSync = require("readline-sync");
-
-let name = readlineSync.question('Can you tell me your name ? ');
-let firstName = readlineSync.question('Can you tell me your First name ? ');
-let city = readlineSync.question('Can you tell me where do you live ? ');
-
-console.log("your name is" , name , firstName , "and you live in" , city);
