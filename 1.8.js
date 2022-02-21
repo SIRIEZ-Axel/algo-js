ab4f4910e61022680fabd56e79de6ba90fac6d94
SIRIEZ-Axel <98603007+SIRIEZ-Axel@users.noreply.github.com>
2022-02-21 14:47:38 +0100

    Delete 1.8.js

--- 1.8.js
+++
@@ -1,7 +0,0 @@
-const readlineSync = require("readline-sync");
-
-let question1 = readlineSync.question('what is your biggest dream ? ')
-let question2 = readlineSync.question('what is your best drunk memory ? ')
-let question3 = readlineSync.question('how did you lose your first tooth ? ')
-
-console.log("So u want",  question1 , "on" , question2 , "with" , question3)
