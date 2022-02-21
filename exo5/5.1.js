9ef4a14cc66b9d0974e0239fb70213b4673890ad
Axel Siriez <axel.siriez@gmail.com>
2022-02-21 14:44:51 +0100

    exo 5.1

---
+++ exo5/5.1.js
@@ -0,0 +1,17 @@
+let readlineSync = require("readline-sync");
+
+function askTvseries() {
+    let name = (readlineSync.question("Tell me what's your favorite Tv show: "));
+    let production = (readlineSync.question("When did the production begin: "));
+    let cast = (readlineSync.question("Give me the names of the cast members: "));
+        let Tvobject = {
+            "Title of tv show " : name,
+            "The production year " : production,
+            "Members cast " : cast
+        };
+        return Tvobject;
+}
+
+let result = JSON.stringify(askTvseries())
+
+console.log(result)
